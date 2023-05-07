b8cab22a50f2613193da3d3a400e7547be036967
Karol Wiśniewski <karol.wisniewski2901@gmail.com>
2023-05-07 21:51:44 +0200

    add folders sort

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ class FileOrganizer:
                 files.append(file)
         return files
 
+    def get_folders(self):
+        folders = []
+        for folder in os.listdir(self.folder_path_abs):
+            if os.path.isfile(os.path.join(self.folder_path_abs, folder)) != True:
+                folders.append(folder)
+        return folders
+
     def get_extension(self, file):
         return os.path.splitext(file)[1]
 
@@ -35,6 +42,10 @@ class FileOrganizer:
             list_folders.append(extension)
         return list_folders
 
+    def create_folder(self):
+        os.makedirs(os.path.join(
+            self.folder_path_abs, 'folders'), exist_ok=True)
+
     def move_files(self, files, list_folders):
         for file in files:
             extension = self.get_extension(file)
@@ -48,16 +59,32 @@ class FileOrganizer:
                         print("[ERROR] Can't move {} file.".format(file))
                     break
 
-    def stort_by_extension(self):
-        files = self.get_files()
-        extensions = self.get_list_extensions(files)
-        list_folders = self.create_folders(extensions)
-        self.move_files(files, list_folders)
+    def move_folders(self, folders):
+        for folder in folders:
+            try:
+                shutil.move(
+                    "{}\{}".format(self.folder_path_abs, folder),
+                    "{}\{}".format(self.folder_path_abs, 'folders'))
+            except:
+                print("[ERROR] Can't move {} file.".format(folder))
+
+    def sort_by_extension(self, folder:bool=True):
+        try:
+            if folder:
+                folders = self.get_folders()
+                self.create_folder()
+                self.move_folders(folders)
+            files = self.get_files()
+            extensions = self.get_list_extensions(files)
+            list_folders = self.create_folders(extensions)
+            self.move_files(files, list_folders)
+        except:
+            print("[ERROR] Can't sort.")
 
 
 def main():
     file_organizer = FileOrganizer('test')
-    file_organizer.stort_by_extension()
+    file_organizer.sort_by_extension(folder=False)
 
 
 if __name__ == '__main__':
